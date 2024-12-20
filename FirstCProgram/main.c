4ed9dfa14e2ff24d04858cf5e7468944c5158a12
Daniel Peter <peterdanielfun24@gmail.com>
2024-12-20 08:56:41 +0100

    c here i come

---
+++ FirstCProgram/main.c
@@ -0,0 +1,14 @@
+// Display and input on console
+// Date: 20/12/2024
+// Author: Peter Daniel
+#include <stdio.h>
+
+int main()
+{
+    int a;
+    printf("hello, world \n");
+    printf("please give the input \n");
+    scanf("%d",&a);
+    printf("value is %d", a);
+    return 0;
+}
