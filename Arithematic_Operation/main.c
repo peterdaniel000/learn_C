9d465c09ab9720e5e6fffd2c30e2abdbb2454d06
Daniel Peter <peterdanielfun24@gmail.com>
2024-12-22 16:15:10 +0100

    more to go

---
+++ Arithematic_Operation/main.c
@@ -0,0 +1,22 @@
+//To learn the arithematic operation of c programming language
+//Date: 22-12-2024
+//Author: Peter Daniel
+
+#include <stdio.h>
+
+int main()
+{
+    int year;
+    printf("Enter the year \n");
+    scanf("%d", &year);
+    //printf("the year you wrote is %d", year);
+    if(((year % 4 == 0)&&(year % 100 != 0)) ||(year%400 == 0))
+    {
+        printf("Year %d is a leap year \n", year);
+    }
+    else
+    {
+        printf("year %d is not a leap year \n", year);
+    }
+    return 0;
+}
